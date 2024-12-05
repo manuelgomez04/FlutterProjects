542596e6bcfeb1d8a38cb6a6faca3f492bc271b2
Manuel <gomez.maman24@triana.salesianos.edu>
2024-12-05 13:58:15 +0100

    hecho

--- disenio_ui_login/lib/login_page.dart
+++ disenio_ui_login/lib/login_page.dart
@@ -6,7 +6,16 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Login Page')),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFE62F16),
+          title: const Text(""),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
+          ],
+        ),
         body: Container(
           color: const Color(0xFFE62F16),
           child: SizedBox(
@@ -16,32 +25,100 @@ class LoginPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  margin: const EdgeInsets.only(bottom: 125),
+                  margin: const EdgeInsets.only(bottom: 170),
                   child: const Column(
                     children: [
                       Image(
                         image: AssetImage('assets/path_logo.png'),
-                        width: 300,
+                        width: 250,
                       ),
-                      Text("Beautiful, Private Sharing")
+                      Text.rich(
+                        TextSpan(
+                          text: "Beautiful, Private Sharing",
+                          style: TextStyle(
+                              color: Color.fromARGB(150, 224, 224, 209),
+                              fontSize: 16),
+                        ),
+                      ),
                     ],
                   ),
                 ),
                 ElevatedButton(
                   onPressed: () {},
-                  child: const Text('Log In'),
+                  child: const Padding(
+                    padding: EdgeInsets.only(
+                        left: 100, right: 100, top: 15, bottom: 15),
+                    child: Text.rich(
+                      TextSpan(
+                        text: "Sign up",
+                        style: TextStyle(
+                            color: Color(0xFFE62F16),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
                 ),
                 const SizedBox(height: 20),
-                const Text("Already have a Path account?"),
-                const SizedBox(height: 20),
-                ElevatedButton(onPressed: () {}, child: const Text("Sing up")),
+                const Text.rich(
+                  TextSpan(
+                    text: "Already have a Path account?",
+                    style: TextStyle(
+                        color: Color.fromARGB(100, 224, 224, 209),
+                        fontSize: 14),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                          color: Color.fromARGB(100, 224, 224, 209)),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.only(
+                          left: 100, right: 100, top: 15, bottom: 15),
+                      child: Text.rich(
+                          TextSpan(
+                            text: "Log in",
+                            style: TextStyle(
+                                color: Color.fromARGB(100, 224, 224, 209),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                          selectionColor: Color(0xFFE62F16)),
+                    )),
                 const SizedBox(height: 20),
                 Container(
-                  margin: const EdgeInsets.only(bottom: 50),
+                  margin: const EdgeInsets.only(bottom: 100),
                   child: const SizedBox(
-                    width: 300,
-                    child: Text(
-                      "By using Path, you agree to our Terms of Service and Privacy Policy",
+                    width: 260,
+                    child: Text.rich(
+                      TextSpan(
+                          text: "By using Path, you agree to Path's ",
+                          style: TextStyle(
+                            color: Color.fromARGB(100, 224, 224, 209),
+                            fontSize: 14,
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: "Terms of Use",
+                                style: TextStyle(
+                                  color: Color.fromARGB(221, 245, 245, 240),
+                                  decoration: TextDecoration.underline,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            TextSpan(
+                              text: " and ",
+                            ),
+                            TextSpan(
+                                text: "Privacy Policy",
+                                style: TextStyle(
+                                  color: Color.fromARGB(221, 245, 245, 240),
+                                  decoration: TextDecoration.underline,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ]),
                       textAlign: TextAlign.center,
                     ),
                   ),
